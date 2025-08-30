255111df51bd19fe4e1cf484adfe093b9d27a544
prabhukd <prabhukrupadwibedy@gmail.com>
2025-08-30 09:26:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #Api Keys here
-ASSEMBLY_KEY = "a95ab516fb9f491a8958480fdfea7a1c"
-GEMINI_KEY = "AIzaSyBDE7-guGp9WpPNpOzWTCaagazn_GCK2cw "
-MURF_KEY = "ap2_cd712df0-ee7d-4848-9eeb-d8a8ab3627a8"
+ASSEMBLY_API_KEY = "a95ab516fb9f491a8958480fdfea7a1c"
+GEMINI_API_KEY = "AIzaSyBDE7-guGp9WpPNpOzWTCaagazn_GCK2cw "
+MURF_API_KEY = "ap2_cd712df0-ee7d-4848-9eeb-d8a8ab3627a8"
